8a1b35ce33e5362cdb6e4a83062bae561179c0a4
Oleg Sucharevich <olegs@codefresh.io>
2018-08-07 09:13:02 +0300

    rename test function

--- pkg/dal/dal_test.go
+++ pkg/dal/dal_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestWhenPathNoSet(t *testing.T) {
+func TestNewDalFromFilePathWhenPathNoSet(t *testing.T) {
 	d := NewDalFromFilePath("")
 	if len(d.Destinations) > 0 || len(d.Filters) > 0 || len(d.Integrations) > 0 {
 		t.Errorf("Should be empty")
