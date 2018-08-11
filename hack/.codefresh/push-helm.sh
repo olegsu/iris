057429e33840cd0e30a1bebd76093b08d8c0f485
Oleg Sucharevich <olegs@codefresh.io>
2018-08-11 19:50:18 +0300

    Add push to helm repo scripts from CF pipeline
    * add helm push script
    
    * remove the version from the chart
    
    * promote version to 0.0.2
    
    * update default image tag in helm chart

---
+++ hack/.codefresh/push-helm.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+defaultBranch="master"
+
+getChartVersion(){
+    version="$(cat VERSION)"
+    if [ "$CF_BRANCH_TAG_NORMALIZED" = "$defaultBranch" ]
+    then
+        version=$version
+    else
+        version=$version-$CF_BRANCH_TAG_NORMALIZED-$CF_SHORT_REVISION
+    fi
+    echo $version
+}
+
+updateChartVersion(){
+    version=$1
+    yq --arg version "$version" '.version = $version' $CF_VOLUME_PATH/iris/iris/Chart.yaml | yq -y '.sources[.sources | length] = env.CF_COMMIT_URL' --yaml-output > $CF_VOLUME_PATH/Chart.new.yaml
+    mv $CF_VOLUME_PATH/Chart.new.yaml $CF_VOLUME_PATH/iris/iris/Chart.yaml
+}
+
+v=$(getChartVersion)
+echo "Setting version to be $v"
+updateChartVersion $v
+cf_export IRIS_VERSION=$v
