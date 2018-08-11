7157b90f8a67afc6a6c32e00d3839ee2666b0fff
Oleg Sucharevich <olegs@codefresh.io>
2018-08-11 19:10:57 +0300

    remove version from chart

---
+++ hack/.codefresh/push-helm.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+defaultBranch="master"
+
+getChartVersion(){
+    version="$(cat VERSION)"
+    if [ "$CF_BRANCH_TAG_NORMALIZED" = "$defaultBranch" ]
+    then
+        version=$version
+    else
+        version=$version-$CF_BRANCH_TAG_NORMALIZED-$CF_SHORT_REVISION
+    fi
+    echo $version
+}
+
+updateChartVersion(){
+    version=$1
+    yq '.version = env.PACKAGE_VERSION' ${{CF_VOLUME_PATH}}/iris/iris/Chart.yaml | yq -y '.sources[.sources | length] = env.CF_COMMIT_URL' --yaml-output > ${{CF_VOLUME_PATH}}/Chart.new.yaml
+    mv ${{CF_VOLUME_PATH}}/Chart.new.yaml ${{CF_VOLUME_PATH}}/iris/iris/Chart.yaml
+}
+
+v=$(getChartVersion)
+echo "Setting version to be $v"
+cat ${{CF_VOLUME_PATH}}/iris/iris/Chart.yaml
